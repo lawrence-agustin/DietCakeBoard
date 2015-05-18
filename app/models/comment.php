04783ef685e218dc22ff48fae317912c0cda4046
lawrence-agustin <l.agustin@klab.com>
2015-05-18 10:14:03 +0800

    Remove commented codes in models.php

--- app/models/comment.php
+++ app/models/comment.php
@@ -14,25 +14,6 @@ class Comment extends AppModel
         ),
     );
 
-    // public static function getAll($thread_id)
-    // {
-    //     $comment = array();
-    //     $db = DB::conn();
-    //     $rows = $db->rows("SELECT * FROM comment WHERE thread_id = ?", $thread_id);
-
-    //     foreach($rows as $row){
-    //         $comment[] = new Comment($row);
-    //     }
-
-    //     return $comment;
-    // }
-
-    // public static function getUserId($username)
-    // {
-    //     $db = DB::conn();
-    //     $row = $db->row("SELECT user_id FROM user WHERE username = ?", $username);
-    //     return $row;
-    // }
 
 
 }
